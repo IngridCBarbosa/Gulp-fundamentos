339129697048e837bf72d54e6f5206498120fa72
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-07-09 18:20:02 -0300

    finished spa using gulp

--- spa/gulpTasks/servidor.js
+++ spa/gulpTasks/servidor.js
@@ -1,11 +1,24 @@
-const gul = require('gulp');
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+const watch = require('gulp-watch');
 
 
-function monitorarArquivos(callback){
-    return callback();
+function monitorarArquivos() {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 3333,
+            open: true,
+            livereload: true,
+        }))
 }
 
-function servidor(callback){
+function servidor(callback) {
+    
+    watch('src/**/*.html', () => gulp.series('appHTML')());
+    watch('src/**/*.scss', () => gulp.series('appCSS')());
+    watch('src/**/*.js', () => gulp.series('appJS')());
+    watch('src/assets/img/**/*.*', () => gulp.series('appIMG'));
+    
     return callback();
 }
 
